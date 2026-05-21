caaf63e8fb0a3b5c2757c284d450b553870cb672
Luiz Estrella <luizeduardoestrella@gmail.com>
2026-05-21 13:45:48 -0300

    feat: temporarily disable access to the public sector version

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { lazy, Suspense } from 'react'
 
 const Home = lazy(() => import('./pages/Home'))
-const IciaGov = lazy(() => import('./pages/IciaGov'))
 
 function Loading() {
   return (
@@ -18,7 +17,8 @@ export default function App() {
       <Suspense fallback={<Loading />}>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/icia-gov" element={<IciaGov />} />
+          <Route path="/icia-gov" element={<Navigate to="/" replace />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
